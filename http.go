5cdd67f0cdb083ed67aaa28e442ba0879d80d26b
kiritoxkiriko <wangxinmeng1997@hotmail.com>
2024-07-01 02:27:34 +0800

    ref: add missing comments

--- http.go
+++ http.go
@@ -12,6 +12,7 @@ const (
 	DefaultBasePath = "/_comical-kv/"
 )
 
+// HTTPPool is a pool of HTTP servers
 type HTTPPool struct {
 	// self is the address of this node
 	self string
@@ -21,6 +22,7 @@ type HTTPPool struct {
 	engine *comical.Engine
 }
 
+// NewHTTPPool creates a new HTTPPool
 func NewHTTPPool(self string) *HTTPPool {
 	h := &HTTPPool{
 		self:     self,
@@ -31,13 +33,18 @@ func NewHTTPPool(self string) *HTTPPool {
 	return h
 }
 
+// Log logs a message
 func (p *HTTPPool) Log(format string, v ...any) {
 	log.Printf("[Comical-KV Server %s] %s\n", p.self, fmt.Sprintf(format, v...))
 }
 
+// registerRoute registers the route for the HTTPPool
 func (p *HTTPPool) registerRoute() {
+	// register the route for the view
 	p.engine.GET(p.basePath+":groupName/:key", func(c *comical.Context) {
 		p.Log("%s %s", c.Method, c.Path)
+
+		// get the group name and key from the path
 		groupName := c.Param("groupName")
 		if groupName == "" {
 			c.Fail(http.StatusBadRequest, "bad request, no group name")
@@ -49,23 +56,27 @@ func (p *HTTPPool) registerRoute() {
 			return
 		}
 
+		// get the group
 		group, ok := GetGroup(groupName)
 		if !ok {
 			c.Fail(http.StatusNotFound, fmt.Sprintf("no such group: %s", groupName))
 			return
 		}
 
+		// get the view
 		view, err := group.Get(key)
 		if err != nil {
 			c.Err(err)
 			return
 		}
 
+		// return the view
 		c.SetHeader("Content-Type", "application/octet-stream")
 		c.Data(200, view.ByteSlice())
 	})
 }
 
+// ServeHTTP serves the HTTPPool
 func (p *HTTPPool) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	p.engine.ServeHTTP(w, r)
 }
